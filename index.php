a3cb998d2dfa1e2f7aa15888e29509373270b29f
Daiyuki <d.maiafujiyoshi@gmail.com>
2020-09-10 11:51:30 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -27,9 +27,34 @@ $busca = Usuario::search("yuki");
 echo json_encode($busca);
 */
 
+/*
 //carrega um usuário usando o login e a senha
 $usuario = new Usuario(); //instanciou (criou um objeto)
 $usuario->login("daiyuki","dmj140891");
+echo $usuario;
+*/
+
+
+/*
+//Inserir novo usuario - versão sem construtor
+$aluno = new Usuario();
+$aluno->setDeslogin("yukio");
+$aluno->setDessenha("yukio220361");
+$aluno->insert();
+echo $aluno; //mostrar na tela
+*/
+
+/*
+//Inserir novo usuario - versão com construtor
+$aluno = new Usuario("YDHN","ydhn.1987");
+$aluno->insert();
+echo $aluno; //mostrar na tela
+*/
+
+//Atualizando dados de um usuário
+$usuario = new Usuario();
+$usuario->loadById(16); //carregamos o usuario id=16
+$usuario->update("professor","prof123");
 echo $usuario;
 
 ?>
